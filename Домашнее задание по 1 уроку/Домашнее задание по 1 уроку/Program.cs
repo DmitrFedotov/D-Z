6b0690268d265fedc07bbb2200be38097022d868
Dmitriy <dimukai@yandex.ru>
2021-10-26 23:05:51 +0400

    Это первое домашнее задание

---
+++ Домашнее задание по 1 уроку/Домашнее задание по 1 уроку/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Домашнее_задание_по_1_уроку
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя");
+            String userName = Console.ReadLine();
+            Console.WriteLine($"Привет {userName} {DateTime.Now}" ); // Тут добавленно настоящее время и дата когда вы просматриваете домашнее задание
+        }
+    }
+}
