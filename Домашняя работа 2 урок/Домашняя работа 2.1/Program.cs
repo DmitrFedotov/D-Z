2a068ccd213e8b1cce25c38717ebf3fa367a2d00
Dmitriy <dimukai@yandex.ru>
2021-10-30 12:23:06 +0400

    часть домашней работы ко 2 уроку

---
+++ Домашняя работа 2 урок/Домашняя работа 2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Домашняя_работа_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+
+                double one, two;
+
+                Console.WriteLine("Введите минимальную t°C");
+                one = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите максимальную t°C");
+                two = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Средняе суточная t°C в сутки {(one + two) / 2}");
+            }
+        }
+    }
+}
