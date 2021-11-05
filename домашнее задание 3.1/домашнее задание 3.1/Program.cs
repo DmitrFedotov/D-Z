f24ce7bfba75688b16ee94b873330ce1403b4f48
Dmitriy <dimukai@yandex.ru>
2021-11-05 19:39:38 +0400

    не понял как вывести текст в задании 2 , ввод успешен и вывод , а что и куда вписывать ,неполучается вывести тест

---
+++ домашнее задание 3.1/домашнее задание 3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace домашнее_задание_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[5,2];
+            Random random = new Random();
+            for (int q = 0; q < myArray.GetLength(0); q++)
+            {
+                for (int w = 0; w < myArray.GetLength(1); w++)
+                {
+                    myArray[q,w] = random.Next( 100, 150 );
+                }
+                Console.WriteLine();
+
+            }
+
+
+
+            for (int x = 0; x < myArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < myArray.GetLength(1); y++)
+                {
+                    Console.Write(myArray[x,y] + "\t");
+                }
+                Console.WriteLine();
+
+            }
+
+
+
+        }
+    }
+}
---
+++ домашнее задание 3.1/домашнее задание 3.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace домашнее_задание_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите данные пользователя");
+            string[,] book = new string[,]
+            {
+              
+              { "\n Ф.И.О пользователя: ", Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine()},
+              { "\n Номер или e-mail : ", Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine()}
+            
+            };
+            Console.WriteLine("Ваш справочник");
+            for (int i = 0; i < book.GetLength(0); i++)
+            {
+               
+                for (int q = 0; q < book.GetLength(1); q++)
+                {
+                    
+                    Console.Write(book[i, q] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
